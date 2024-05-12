631e9d7dddafcd2d34bc12cbf1d140ef740e5c09
James <james@yieldstudio.fr>
2024-05-12 23:36:39 +0200

    chore: configure dev tools

--- config/settings.php
+++ config/settings.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
-    'api_base_url' => env('API_BASE_URL')
+    'api_base_url' => env('API_BASE_URL'),
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/test/{id}', function (string $id) {
-    return view('test', ['id'=>$id]);
+    return view('test', ['id' => $id]);
 })->name('test');
